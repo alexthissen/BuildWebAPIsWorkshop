50066380a04ec26da3194fe5d9d21716c24ee6a0
Alex Thissen <athissen@xpirit.com>
2018-04-13 23:34:15 +0200

    Split CreateDefaultBuilder implementation into separate parts Corrected developer base URL

--- GenealogyWebAPI/Program.cs
+++ GenealogyWebAPI/Program.cs
@@ -17,8 +17,29 @@ namespace GenealogyWebAPI
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            //return WebHost.CreateDefaultBuilder();
+            var builder = new WebHostBuilder()
+                .UseKestrel((builderContext, options) =>
+                {
+                    options.Configure(builderContext.Configuration.GetSection("Kestrel"));
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseDefaultServiceProvider((context, options) =>
+                {
+                    options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                })
                 .UseStartup<Startup>();
+
+            if (args != null)
+            {
+                builder.UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build());
+            }
+
+            return builder;
+
+        }
     }
 }
--- GenealogyWebAPI/Startup.cs
+++ GenealogyWebAPI/Startup.cs
@@ -15,9 +15,15 @@ namespace GenealogyWebAPI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
