6ba4f35d8412218f99806ed4a61dfcba52c82c8e
Alex Thissen <athissen@xpirit.com>
2018-04-14 00:23:13 +0200

    Configured API options for more detailed validation error handling

--- GenealogyWebAPI/Startup.cs
+++ GenealogyWebAPI/Startup.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -51,13 +52,36 @@ namespace GenealogyWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // Options for particular external services
             services.Configure<GenderizeApiOptions>(Configuration.GetSection("GenderizeApiOptions"));
 
             ConfigureHealth(services);
             ConfigureOpenApi(services);
+            ConfigureApiOptions(services);
+        }
+
+        private void ConfigureApiOptions(IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Instance = context.HttpContext.Request.Path,
+                        Status = StatusCodes.Status400BadRequest,
+                        Type = "https://asp.net/core",
+                        Detail = "Please refer to the errors property for additional details."
+                    };
+                    return new BadRequestObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json", "application/problem+xml" }
+                    };
+                };
+            });
         }
 
         private void ConfigureOpenApi(IServiceCollection services)
