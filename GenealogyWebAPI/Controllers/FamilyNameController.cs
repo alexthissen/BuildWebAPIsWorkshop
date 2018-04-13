fe15cb0039782d5cea07db41047054e820fda14f
Alex Thissen <athissen@xpirit.com>
2018-04-13 22:54:02 +0200

    Added Refit and FamilyName controller to Genderize.io API

---
+++ GenealogyWebAPI/Controllers/FamilyNameController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GenealogyWebAPI.Proxies;
+using Refit;
+
+namespace GenealogyWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FamilyNameController : ControllerBase
+    {
+        // GET api/familyname/name
+        [HttpGet("{name}")]
+        public async Task<ActionResult<string>> Get(string name)
+        {
+            string result = null;
+            try
+            {
+                string baseUrl = "https://api.genderize.io/";
+                result = await RestService.For<IGenderizeClient>(baseUrl).GetGenderForName(name);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ GenealogyWebAPI/Proxies/IGenderizeClient.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenealogyWebAPI.Proxies
+{
+    // https://api.genderize.io/?name=igor&country_id=ua
+    // https://genderize.io/
+
+    [Headers("User-Agent: Genderize IO WebAPI Client 1.0")]
+    public interface IGenderizeClient
+    {
+        [Get("/")]
+        Task<string> GetGenderForName(string name);
+    }
+}
