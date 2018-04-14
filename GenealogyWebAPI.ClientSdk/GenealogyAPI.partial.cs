0c5764836f282fbe9e7a5ae570c8f12a1642bc6a
Alex Thissen <athissen@xpirit.com>
2018-04-14 10:15:37 +0200

    Created test stack for contract testing

---
+++ GenealogyWebAPI.ClientSdk/GenealogyAPI.partial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace GenealogyWebAPI.ClientSdk
+{
+    public partial class GenealogyAPI
+    {
+        public GenealogyAPI(HttpClient client) : base(client, false)
+        {
+            Initialize();
+            BaseUri = client.BaseAddress;
+        }
+    }
+}
--- GenealogyWebAPI.IntegrationTests/ServiceContractIntegrationTests.cs
+++ GenealogyWebAPI.IntegrationTests/ServiceContractIntegrationTests.cs
@@ -1,3 +1,4 @@
+using GenealogyWebAPI.ClientSdk;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,5 +10,21 @@ namespace GenealogyWebAPI.IntegrationTests
     [TestClass]
     public class ServiceContractIntegrationTests
     {
+        TestServer server;
+        HttpClient client;
+        GenealogyAPI proxy;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var builder = new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<Startup>();
+
+            // Create test stack
+            server = new TestServer(builder);
+            client = server.CreateClient();
+            proxy = new GenealogyAPI(client);
+        }
     }
 }
