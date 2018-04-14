7bdf686b5693b86843a9ad7af7395db62942fbbd
Alex Thissen <athissen@xpirit.com>
2018-04-14 10:06:29 +0200

    Added integration tests project

---
+++ GenealogyWebAPI.IntegrationTests/ServiceContractIntegrationTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+
+namespace GenealogyWebAPI.IntegrationTests
+{
+    [TestClass]
+    public class ServiceContractIntegrationTests
+    {
+    }
+}
